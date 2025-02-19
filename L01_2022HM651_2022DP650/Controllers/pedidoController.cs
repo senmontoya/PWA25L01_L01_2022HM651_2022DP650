e6b799bbb4abfe625261e570f44e32a603c84996
senmontoya <alfredo.hernandez@catolica.edu.sv>
2025-02-18 19:07:36 -0600

    Contexto y models (pedidos, cliente) agregados

---
+++ L01_2022HM651_2022DP650/Controllers/pedidoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using L01_2022HM651_2022DP650.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_2022HM651_2022DP650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class pedidoController : ControllerBase
+    {
+        private readonly restauranteContext _restauranteContexto;
+
+        public pedidoController(restauranteContext restauranteContexto)
+        {
+            _restauranteContexto = restauranteContexto;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarPedido([FromBody] pedido pedido)
+        {
+            try
+            {
+                _restauranteContexto.pedido.Add(pedido);
+                _restauranteContexto.SaveChanges();
+                return Ok("Pedido guardado");
+            }
+            catch (Exception bug)
+            {
+                return BadRequest(bug.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Update/{id}")]
+        public IActionResult ActualizarPedido(int id, [FromBody] pedido pedido)
+        {
+            try
+            {
+                pedido? pedidoActual = (from p in _restauranteContexto.pedido where p.pedidoId == id select p).FirstOrDefault();
+                if (pedidoActual == null)
+                {
+                    return NotFound("Pedido no encontrado");
+                }
+                pedidoActual.motoristaId = pedido.motoristaId;
+                pedidoActual.clienteId = pedido.clienteId;
+                pedidoActual.platoId = pedido.platoId;
+                pedidoActual.cantidad = pedido.cantidad;
+                pedidoActual.precio = pedido.precio;
+                _restauranteContexto.Entry(pedidoActual).State = EntityState.Modified;
+                _restauranteContexto.SaveChanges();
+                return Ok("Pedido actualizado");
+            }
+            catch (Exception bug)
+            {
+                return BadRequest(bug.Message);
+            }
+        }
+
+    }
+}
---
+++ L01_2022HM651_2022DP650/Models/pedido.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2022HM651_2022DP650.Models
+{
+    public class pedido
+    {
+        [Key]
+        public int pedidoId { get; set; }
+        public int? motoristaId { get; set; }
+        public int? clienteId { get; set; }
+        public int? platoId { get; set; }
+        public int? cantidad { get; set; }
+        public decimal? precio { get; set; }
+
+    }
+}
