b57ba21a73173f7d14e11a598f5b1ed9d9493f5a
senmontoya <alfredo.hernandez@catolica.edu.sv>
2025-02-18 18:39:13 -0600

    Agregar archivos de proyecto.

---
+++ L01_2022HM651_2022DP650/Models/restauranteContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace L01_2022HM651_2022DP650.Models
+{
+    public class restauranteContext : DbContext
+    {
+        public restauranteContext(DbContextOptions<restauranteContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
